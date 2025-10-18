8cb2315a27c7539b1509369f397c6e592396ae5f
Maysara <maysaracs1001@gmail.com>
2025-10-18 13:54:58 +0300

    database connected

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // ✅ Add this
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Dependency Injection (DI)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DevConnection"),
+        new MySqlServerVersion(new Version(8, 0, 0)) // ✅ MySQL version
+    )
+);
 
 var app = builder.Build();
 
@@ -10,14 +22,18 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
+
+// EF Core commands to use:
+// Add-Migration "InitialCreate"
+// Update-Database
